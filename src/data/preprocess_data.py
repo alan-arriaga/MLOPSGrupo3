2bebe44886ce1c5e7614a51bdd281d28d280213b
macevedoham <156972332+macevedoham@users.noreply.github.com>
2024-11-03 17:11:11 -0600

    Adding requirements and seed for reproducibility (#7)
    * Adding requirements and seed for reproducibility
    
    * Adding requirements and seed for reproducibility
    
    ---------
    
    Co-authored-by: manuel <macevedoham@gmail.com>

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
 import sys
+import random
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, PowerTransformer
 
+RANDOM_SEED = 42 
+
 def preprocess_data(data_path):
+    random.seed = (RANDOM_SEED)
+    np.random.seed = (RANDOM_SEED)
+
     data = pd.read_csv(data_path)
     X = data.drop('shares', axis=1)
     y = data['shares']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=RANDOM_SEEDcd)
 
     # Transformaciones a columnas en X_train
     X_train['n_tokens_content'] = np.log1p(X_train['n_tokens_content'])
